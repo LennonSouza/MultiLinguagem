616de029e9ae1fc5d8ff0a82539c94da2809e5d3
LennonSouza <lennonsouza2009@gmail.com>
2024-01-18 21:32:12 -0300

    Sequencia de Fibonacci em CSharp

---
+++ CSharp/SequenciaDeFibonacci/Fibonacci.cs
@@ -0,0 +1,27 @@
+﻿namespace SequenciaDeFibonacci;
+
+public class Fibonacci
+{
+    public static void Main(string[] args)
+    {
+        int param1 = 1, param2 = 1, resultado = 1;
+
+        Console.Write("Digite o valor minimo: ");
+        int minimo = int.Parse(Console.ReadLine()) - 1;
+
+        Console.Write("Digite o valor maximo: ");
+        int maximo = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < maximo; i++)
+        {
+            param1 = param2;
+            param2 = resultado;
+            resultado = param1 + param2;
+
+            if (i >= minimo && i <= maximo) 
+            {
+                Console.WriteLine($"{param1} + {param2} = {resultado}");
+            }
+        }
+    }
+}
